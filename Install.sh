192fb3ea7d2c20a9e5100f49649cb8f6bdf88b62
SATHAN <sathanic498@gmail.com>
2025-08-27 16:55:15 +0530

    5 G

--- Install.sh
+++ Install.sh
@@ -1,5 +1,11 @@
-#!/data/data/com.termux/files/usr/bin/bash
-set -e
-install -Dm755 s5gl "$PREFIX/bin/s5gl"
-echo "Installed: $PREFIX/bin/s5gl"
-echo "Run: s5gl help"
+#!/bin/bash
+# Installer for SATHANIC 5G BAND LOCKER
+
+echo "[*] Installing SATHANIC 5G BAND LOCKER..."
+
+# script copy to bin
+chmod +x s5gl
+cp s5gl $PREFIX/bin/s5gl
+
+echo "[+] Installed successfully!"
+echo "    Run: s5gl help"
